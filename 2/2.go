214ba2447c52796b986ba7c490477b12e5689522
kennedn <kennedn@msn.com>
2023-12-02 10:30:38 +0000

    day 2 more cleanup

--- 2/2.go
+++ 2/2.go
@@ -17,20 +17,14 @@ func one(lines []string) {
 	count := 0
 
 	for i, line := range lines {
-		line = strings.Split(line, ": ")[1]
-		sets := strings.Split(line, "; ")
+		line = strings.ReplaceAll(strings.Split(line, ": ")[1], "; ", ", ")
 		line_id := i + 1
-		for _, set := range sets {
-			for _, pair := range strings.Split(set, ", ") {
-				var color string
-				var n int
-				_, _ = fmt.Sscanf(pair, "%d %s", &n, &color)
-				if n > maxCubes[color] {
-					line_id = 0
-					break
-				}
-			}
-			if line_id == 0 {
+		for _, pair := range strings.Split(line, ", ") {
+			var color string
+			var n int
+			_, _ = fmt.Sscanf(pair, "%d %s", &n, &color)
+			if n > maxCubes[color] {
+				line_id = 0
 				break
 			}
 		}
@@ -45,26 +39,21 @@ func two(lines []string) {
 	count := 0
 
 	for _, line := range lines {
-		line = strings.Split(line, ": ")[1]
-		sets := strings.Split(line, "; ")
-		power := 0
+		line = strings.ReplaceAll(strings.Split(line, ": ")[1], "; ", ", ")
 		minCubes := map[string]int{
 			"red":   0,
 			"green": 0,
 			"blue":  0,
 		}
-		for _, set := range sets {
-			for _, pair := range strings.Split(set, ", ") {
-				var color string
-				var n int
-				_, _ = fmt.Sscanf(pair, "%d %s", &n, &color)
-				if n > minCubes[color] {
-					minCubes[color] = n
-				}
+		for _, pair := range strings.Split(line, ", ") {
+			var color string
+			var n int
+			_, _ = fmt.Sscanf(pair, "%d %s", &n, &color)
+			if n > minCubes[color] {
+				minCubes[color] = n
 			}
-			power = minCubes["red"] * minCubes["green"] * minCubes["blue"]
 		}
-		count += power
+		count += minCubes["red"] * minCubes["green"] * minCubes["blue"]
 	}
 	fmt.Printf("Count: %d\n", count)
 }
